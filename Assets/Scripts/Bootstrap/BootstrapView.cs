9d54ea688fb6b38a89b9b429639c837ad84fb0d2
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-06-17 15:58:07 +0300

    Added imageAnimation in Bootstrap scene.

--- Assets/Scripts/Bootstrap/BootstrapView.cs
+++ Assets/Scripts/Bootstrap/BootstrapView.cs
@@ -9,9 +9,23 @@ public class BootstrapView : MonoBehaviour
     private Slider _slider;
 
     [SerializeField]
+    private Image _progressImage;
+
+    [SerializeField]
     private TMP_Text _progressText;
 
     private int _currentProgress = 0;
+    private Tween _imageTween;
+
+    private void Start()
+    {
+        AnimationImage();
+    }
+
+    private void AnimationImage()
+    {
+        _imageTween = _progressImage.rectTransform.DOScale(1.2f, 0.6f).SetEase(Ease.OutBack).SetLoops(10, LoopType.Yoyo);
+    }
 
     public void SetProgress(int progress)
     {
@@ -23,6 +37,14 @@ public class BootstrapView : MonoBehaviour
             _slider.value = x / 100f;
             _progressText.text = $"{x}%";
         }, progress, (progress - _currentProgress) * 0.02f).SetEase(Ease.Linear);
+    }
+
+    private void OnDestroy()
+    {
+        if (_imageTween != null && _imageTween.IsActive())
+        {
+            _imageTween.Kill();
+        }
     }
 }
 
