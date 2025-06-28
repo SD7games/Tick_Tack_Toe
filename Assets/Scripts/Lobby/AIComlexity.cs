492a91a7987cb03db48acae000448c22bde636d4
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-06-28 16:14:27 +0300

    added AiComlexity script

---
+++ Assets/Scripts/Lobby/AIComlexity.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIComlexity : MonoBehaviour
+{
+    [SerializeField]
+    private Button _easyButton;
+
+    [SerializeField]
+    private Button _normButton;
+
+    [SerializeField]
+    private Button _hardButton;
+
+}
