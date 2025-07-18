d4153af74249ae8079b643743a74b27c1a424d5a
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-07-18 13:48:04 +0300

    added Select & Save PlayerName. PlayerSign.

--- Assets/Scripts/Core/Player/PlayerController.cs
+++ Assets/Scripts/Core/Player/PlayerController.cs
@@ -1,7 +1,74 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    
+    [SerializeField]
+    private string _playerName;
+    [SerializeField]
+    private Image _playerSign;
+    [SerializeField]
+    private TMP_InputField _playerInputField;
+    [SerializeField]
+    private EmojiData _emojiData;
+    [SerializeField]
+    private ContentScrollController _contentScrollController;
+
+    private const string PlayerNameKey = "PlayerName";
+    private const string PlayerSpriteKey = "PlayerSprite";
+
+    private void Start()
+    {
+        LoadPlayerData();
+        _playerInputField.onEndEdit.AddListener(SetName);
+        _contentScrollController.OnEmojiSelected += SetPlayerSprite;
+    }
+    private void OnDestroy()
+    {
+        _contentScrollController.OnEmojiSelected -= SetPlayerSprite;
+    }
+
+    public void SetPlayerSprite(Sprite sprite)
+    {
+        _playerSign.sprite = sprite;
+        SavePlayerSprite(sprite);
+    }
+
+    private void SetName(string name)
+    {
+        _playerName = name;
+        PlayerPrefs.SetString(PlayerNameKey, _playerName);
+        PlayerPrefs.Save();
+    }
+
+    private void SavePlayerSprite(Sprite sprite)
+    {
+        PlayerPrefs.SetString(PlayerSpriteKey, sprite.name);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadPlayerData()
+    {
+        if (PlayerPrefs.HasKey(PlayerNameKey))
+        {
+            _playerName = PlayerPrefs.GetString(PlayerNameKey);
+            _playerInputField.text = _playerName;
+        }
+
+        if (PlayerPrefs.HasKey(PlayerSpriteKey))
+        {
+            string spriteName = PlayerPrefs.GetString(PlayerSpriteKey);
+            Sprite foundSprite = _emojiData._emojiSprites.Find(sprite => sprite.name == spriteName);
+
+            if (foundSprite != null)
+            {
+                _playerSign.sprite = foundSprite;
+            }
+            else
+            {
+                Debug.LogWarning("Emoji not found: " + spriteName);
+            }
+        }
+    }
 }
